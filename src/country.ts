c2a8c023b343e04483f560d379074c7dffb72334
nicholidev <nicholijin@outlook.com>
2023-01-12 16:43:27 +0800

    disable flag

--- src/country.ts
+++ src/country.ts
@@ -1,14 +1,10 @@
-import path from 'path';
 import countryList from './assets/country.json';
 import { findEntryByCode } from './utils';
 import { ICountry } from './interface';
 
-const FLAGS_PATH = path.resolve('./assets');
-
 const all = (): ICountry[] => {
 	const countries = countryList;
 	return countries.map((i: ICountry) => {
-		i.flag = path.join(FLAGS_PATH, `3x2/${i.isoCode}.svg`);
 		return i;
 	});
 };
@@ -16,7 +12,6 @@ const all = (): ICountry[] => {
 const getByCode = (isoCode: string): ICountry | undefined => {
 	if (!isoCode) return undefined;
 	const country = findEntryByCode(countryList, isoCode);
-	country.flag = path.join(FLAGS_PATH, `3x2/${isoCode}.svg`);
 	return country;
 };
 
